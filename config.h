bdadb996e2208a712394bba23f505ea6a2ad73d1
Markus Teich <markus.teich@stusta.mhn.de>
2015-06-06 12:32:44 +0200

    solarize colors

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 static const char *colorname[NUMCOLS] = {
-	"#000044",   /* after initialization */
-	"#005577",   /* during input */
-	"#440000",   /* failed/cleared the input */
+	"#2b4d00",   /* after initialization */
+	"#134569",   /* during input */
+	"#661716",   /* failed/cleared the input */
 };
 static const Bool failonclear = False;
